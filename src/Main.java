14f1ea12b2e9cbcc31446e6907299abdbe594f38
Eldiar004 <eldiarro2004@gmail.com>
2022-10-06 22:45:57 +0600

    Eldiar's commit is done!!

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,48 @@
+import Service.DeliveryCompanyService;
 import entities.Customer;
 import entities.DeliveryCompany;
 import entities.Load;
 import entities.Order;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+                                                                             /**
 
+                                                                              THIS ALL CODE I USED FOR TEST!!!
+
+                                                                              */
+//        DeliveryCompanyService test1 = new DeliveryCompanyService();
+//        test1.createDeliveryCompany();
+//        Load load1 = new Load(1,10,1,3);
+//        Load load2 = new Load(4,12,6,5);
+//        Load load3 = new Load(2,20,4,2);
+//        Load load4 = new Load(5,50,9,5);
+//        Order order1 = new Order(200,load1);
+//        Order order2 = new Order(300,load2);
+//        Order order3 = new Order(300,load2);
+//        Order order4 = new Order(300,load2);
+//        List<Order> list = new ArrayList<>();
+//        list.add(order1);
+//        list.add(order2);
+//        list.add(order3);
+//        list.add(order4);
+//        Customer customer1 = new Customer("Eldiar",list);
+//        Customer customer2 = new Customer("Kerez",new ArrayList<>());
+//        Customer customer3 = new Customer("Azi",new ArrayList<>());
+//        Customer customer4 = new Customer("Sam",new ArrayList<>());
+//        Customer customer5 = new Customer("Boki",new ArrayList<>());
+//        List<Customer> customers = new ArrayList<>();
+//        customers.add(customer1);
+//        customers.add(customer2);
+//        customers.add(customer3);
+//        customers.add(customer4);
+//        customers.add(customer5);
+//        test1.getTotalFee(customers);
+//        test1.getCompanyProfit(scanner.nextLine());
     }
 }
